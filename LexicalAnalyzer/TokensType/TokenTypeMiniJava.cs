23b6524d0d742bac07b7da018642ee8bfbdb50fa
FernandoSL <nando.leandro@hotmail.com>
2019-09-27 16:51:44 -0300

    Create MiniJava Token.

--- LexicalAnalyzer/TokensType/TokenTypeMiniJava.cs
+++ LexicalAnalyzer/TokensType/TokenTypeMiniJava.cs
@@ -1,15 +1,94 @@
-﻿namespace AnalisadorLexico.TiposToken
+﻿using System.ComponentModel;
+
+namespace AnalisadorLexico.TiposToken
 {
     public enum TokenTypeMiniJava
     {
+        [Description("class")]
         SCLASS,
+
+        [Description("public")]
         SPUBLIC,
+
+        [Description("static")]
         SSTATIC,
+
+        [Description("void")]
         SVOID,
+
+        [Description("main")]
         SMAIN,
+
+        [Description("{")]
         SABRE_CHAVES,
+
+        [Description("}")]
         SFECHA_CHAVES,
+
+        [Description("(")]
         SABRE_PARENTESES,
+
+        [Description(")")]
         SFECHA_PARENTESES,
+
+        [Description("[")]
+        SABRE_COLCHETES,
+
+        [Description("]")]
+        SFECHA_COLCHETES,
+
+        [Description("extends")]
+        SEXTENDS,
+
+        [Description("int")]
+        SINT,
+
+        [Description(";")]
+        SPONTO_VIRGULA,
+
+        [Description("if")]
+        SIF,
+
+        [Description("else")]
+        SELSE,
+
+        [Description("while")]
+        SWHILE,
+
+        [Description("System.out.println")]
+        SSYSTEM_OUT_PRINTLN,
+
+        [Description("=")]
+        SATRIBUICAO,
+
+        [Description("&&")]
+        SAND,
+
+        [Description("<")]
+        SMENOR,
+
+        [Description("+")]
+        SADICAO,
+
+        [Description("-")]
+        SSUBTRACAO,
+
+        [Description("*")]
+        SMULTIPLICACAO,
+
+        [Description("true")]
+        STRUE,
+
+        [Description("false")]
+        SFALSE,
+
+        [Description("this")]
+        STHIS,
+
+        [Description("new")]
+        SNEW,
+
+        [Description("!")]
+        SEXCLAMACAO
     }
 }
